fd13ccb04676a08c70c698d96784512d71c3ccfd
xeavier <chandarmtsb007@gmail.com>
2020-12-09 12:01:17 +0530

    1 file modified, 3 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ResultsComponent } from './results/results.component';
+import { ResultsComponent } from './pages/results/results.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],

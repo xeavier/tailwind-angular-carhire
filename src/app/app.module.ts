772efd6d1d04f288d4409de6112c2da1c383b539
xeavier <chandarmtsb007@gmail.com>
2020-12-09 11:59:06 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ResultsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
